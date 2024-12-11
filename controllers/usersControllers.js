1ad9f3134b8ee03f95c3531db48f307408f04f13
Simcha Berkovitz <151738331+Simcha-b@users.noreply.github.com>
2024-12-11 17:13:00 +0200

    refactor loginUser function to improve error handling and response messages

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -4,22 +4,6 @@ import pool from "../db/connection.js";
 // מפתח סודי לאימות הטוקן
 const secretKey = process.env.my_secret_key;
 
-// import { getUser } from "../db/queries/usersQueries.js";
-
-// const getUserById = async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const user = await getUser(id);
-//     res.json(user);
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: "Error retrieving user",
-//       error: error.message || "Internal Server Error",
-//     });
-//   }
-// };
-
 async function loginUser(req, res) {
   const { email, password } = req.body;
   // console.log("Login request received:", email, password);
@@ -31,7 +15,10 @@ async function loginUser(req, res) {
     ]);
 
     if (users.length === 0) {
-      throw new Error("User not found");
+      return res.status(401).json({
+        success: false,
+        message: "משתמש לא נמצא"
+      });
     }
 
     const user = users[0];
@@ -40,7 +27,10 @@ async function loginUser(req, res) {
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      throw new Error("Invalid password");
+      return res.status(401).json({
+        success: false,
+        message: "סיסמה שגויה"
+      });
     }
 
     // יצירת טוקן JWT
@@ -62,7 +52,11 @@ async function loginUser(req, res) {
     res.json({ token, userInfo }); 
   } catch (err) {
     console.error("Error logging in:", err);
-    throw err;
+    res.status(500).json({
+      success: false,
+      message: "אירעה שגיאה בתהליך ההתחברות",
+      error: err.message
+    });
   }
 }
 
